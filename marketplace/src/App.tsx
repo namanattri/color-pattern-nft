4235ee737e385e2f9f3bdd12f52d3f17a9dbf3a4
Naman Attri <namanattri5@gmail.com>
2021-10-02 13:29:09 +0530

    create Pattern component

--- marketplace/src/App.tsx
+++ marketplace/src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
+import { Pattern } from './components/pattern/Pattern';
 import './App.css';
-
-function getRandomIntInclusive(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-const getRandomColor = () => {
-  return `rgb(${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)})`
-}
 
 function App() {
   return (
     <div className="App">
-      <div className="pattern">
-        {[...Array(200)].map((e, i) => (<div className="color-cell" style={{backgroundColor: getRandomColor()}}></div>))}
-      </div>
+      <Pattern />
     </div>
   );
 }
---
+++ marketplace/src/components/pattern/Pattern.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Pattern.css';
+
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomColor = () => {
+  return `rgb(${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)})`
+}
+
+export const Pattern = () => {
+  return (
+    <div className="pattern">
+      {[...Array(200)].map((e, i) => (<div className="color-cell" style={{backgroundColor: getRandomColor()}}></div>))}
+    </div>
+  );
+}
