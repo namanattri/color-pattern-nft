e876444d8917b1bfb4a0e0f64afb168d8651ad9e
Naman Attri <namanattri5@gmail.com>
2021-10-02 13:24:07 +0530

    genereate colorful pattern

--- marketplace/src/App.tsx
+++ marketplace/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomColor = () => {
+  return `rgb(${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)})`
+}
+
 function App() {
   return (
     <div className="App">
       <div className="pattern">
-        <div className="color-cell">
-          
-        </div>
+        {[...Array(200)].map((e, i) => (<div className="color-cell" style={{backgroundColor: getRandomColor()}}></div>))}
       </div>
     </div>
   );
